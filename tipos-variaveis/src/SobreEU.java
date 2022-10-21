5579b8ff68cbe91a628177c8c08f71cd3f890037
franchescodsa <franchescodsa@gmail.com>
2022-10-20 21:15:03 -0300

    Usando Scanner

---
+++ tipos-variaveis/src/SobreEU.java
@@ -0,0 +1,30 @@
+import java.util.Locale;
+import java.util.Scanner;
+public class SobreEU {
+    public static void main(String[] args) {
+        // criando o objeto Scanner
+        Scanner scanner= new Scanner(System.in).useLocale((Locale.US) );
+
+        System.out.println("Digite seu nome");
+        String nome =scanner.next();
+
+        System.out.println("Digite seu sobrenome");
+        String sobrenome=scanner.next();
+
+        System.out.println("Digite sua idade");
+        int idade=scanner.nextInt();
+
+        System.out.println("Digite sua altura");
+        double altura=scanner.nextDouble();
+
+        // Imprimindo os dados obtidos pelo ususario
+
+        System.out.println("Ola, me chamo "+nome+" "+sobrenome);
+        System.out.println("Tenho "+idade+" anos");
+        System.out.println("Minha altura é "+altura+"cm");
+
+
+    }
+
+
+}
